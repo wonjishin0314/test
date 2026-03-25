8727640bfa91f3601c08b2077a3fd45a73f363ad
wonjishin0314 <wonjishin0314@github.com>
2026-03-25 12:58:24 +0900

    Bump service worker cache to v2 to force update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ca-calendar-v1';
+const CACHE = 'ca-calendar-v2';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
